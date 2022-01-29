bc5488c5332cb05a637a7e6eb5dbe19df26b8b3e
fdhyunseo <77616700+fdhyunseo@users.noreply.github.com>
2022-01-29 10:59:20 +0100

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm> 
+#include <cmath>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int x = 100000;
+	vector<vector<int>> list;
+
+	for (int i = 2; i <= x; i++) {
+		vector<int> num;
+		int k = i;
+		int j = 2;
+		while(k > 1) {
+			if (k % j == 0) {
+				num.push_back(j);
+				k /= j;
+				j = 2;
+				if (j > x) {
+					num.push_back(i);
+					break;
+				}
+			}
+			else {
+				j++;
+			}
+		}
+		list.push_back(num);
+	}
+
+	for (int i = 0; i < list.size(); i++) {
+		vector<int> num = list[i];
+		cout << i + 2 << " = ";
+		int stack = 1;
+		for (int j = 0; j < num.size(); j++) {
+			cout << num[j] << " ";
+		}
+		cout << "\n";
+	}
+}
